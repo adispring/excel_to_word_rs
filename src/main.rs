411dd86431776c77b58f792eda5b94130c9cc78d
wangzengdi <sunnyadi@163.com>
2024-12-15 12:02:43 +0800

    feat: Heading ok

--- src/main.rs
+++ src/main.rs
@@ -11,12 +11,20 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
             return Err(Box::new(e));
         }
     };
+    let mut file = File::create("output/wrong.docx")?;
     let range = workbook
         .worksheet_range("Sheet1")
         .ok_or("Cannot find sheet")??;
 
+    let style1 = Style::new("Heading1", StyleType::Paragraph).name("Heading 1");
+    let style2 = Style::new("Heading2", StyleType::Paragraph).name("Heading 2");
+    let style3 = Style::new("Heading3", StyleType::Paragraph).name("Heading 3");
+
     // Create a new Word document
-    let mut doc = Docx::new();
+    let mut doc = Docx::new()
+        .add_style(style1)
+        .add_style(style2)
+        .add_style(style3);
 
     for (i, row) in range.rows().enumerate() {
         if i == 0 {
@@ -31,19 +39,25 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
 
         if !level1.is_empty() {
             doc = doc.add_paragraph(
-                Paragraph::new().add_run(Run::new().add_text(level1).bold().size(48).heading1()),
+                Paragraph::new()
+                    .add_run(Run::new().add_text(level1).bold().size(48))
+                    .style("Heading1"), // .page_break_before(true),
             );
         }
 
         if !level2.is_empty() {
             doc = doc.add_paragraph(
-                Paragraph::new().add_run(Run::new().add_text(level2).bold().size(40).heading2()),
+                Paragraph::new()
+                    .add_run(Run::new().add_text(level2).bold().size(40))
+                    .style("Heading2"),
             );
         }
 
         if !level3.is_empty() {
             doc = doc.add_paragraph(
-                Paragraph::new().add_run(Run::new().add_text(level3).bold().size(36).heading3()),
+                Paragraph::new()
+                    .add_run(Run::new().add_text(level3).bold().size(36))
+                    .style("Heading3"),
             );
         }
 
@@ -62,7 +76,7 @@ fn main() -> Result<(), Box<dyn std::error::Error>> {
     }
 
     // Save the document
-    let mut file = File::create("output/output.docx")?;
+
     doc.build().pack(&mut file)?;
 
     println!("Document saved successfully to output/output.docx");
